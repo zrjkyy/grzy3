03623171953fb9984bd4a6024cb010e1c080d316
Hyun <102264485+zrjkyy@users.noreply.github.com>
2022-08-29 07:42:31 +0800

    Add files via upload

---
+++ source/js/footer.js
@@ -0,0 +1,10 @@
+document.write('<!-----------  footer----------->');
+document.write('' +
+    '<center>\n' +
+    '<p style="color:#333;">© 2021-2022 小菜</p>\n' +
+    '</center>\n' +
+    '');
+
+
+
+
